c13777bb981386a81924547dbeb320ae611bd0d5
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-02-26 09:31:59 -0300

    Inclusão de bullets nos códigos #3

--- livro/capitulos/code/capitulo-01/node.h
+++ livro/capitulos/code/capitulo-01/node.h
@@ -1,32 +1,18 @@
 #ifndef ELEMENT_T
 #define ELEMENT_T 1
-// Tipo básico de dado a ser armazenado na estrutura
-typedef int Element;
+typedef int Element; // <1>
 #endif
-
 
 #ifndef NODE_T
 #define NODE_T 1
-// Estrutura básica de um Nó
-typedef struct Node_ {
-	Element value; //armazena o valor contido neste nó
-
-	//apontador para o proximo nó; possivelmente null
-	struct Node_*	next;
+typedef struct Node_ { //<2>
+	Element value; // <3>
+	struct Node_*	next; // <4>
 } Node;
 #endif
 
-// Cria um nó com valor v e next como próximo nó
-Node* makeNode(Element v, Node *next);
-
-// Retorna o valor contido no nó n
-Element getValue(Node *n);
-
-// Retorna o apontador para o próximo nó
-Node* getNext(Node *n);
-
-// Altera o valor contido no nó n
-Node* setValue(Node *n, Element v);
-
-// Faz o apontador de próximo de nó n apontar para newNext
-Node* setNext(Node *n, Node *newNext);
+Node* makeNode(Element v, Node *next); // <5>
+Element getValue(Node *n); // <6>
+Node* getNext(Node *n); // <7>
+Node* setValue(Node *n, Element v); // <8>
+Node* setNext(Node *n, Node *newNext); // <9>
