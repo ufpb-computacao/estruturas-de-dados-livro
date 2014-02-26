7188f11180ac50670064a9d3cff349e2af697a5d
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-02-26 09:20:55 -0300

    Otimização de código para impressão #3
    Os códigos e comentários devem ser otimizados para
    impressão, evitando linhas maiores do que 70 carácteres,
    uso do tab para alinhar elementos. Os comentários devem,
    quando possível, diminuídos.
    
    É possível utilizar bullets, mostarei no próximo commit.

--- livro/capitulos/code/capitulo-01/node.h
+++ livro/capitulos/code/capitulo-01/node.h
@@ -1,44 +1,32 @@
 #ifndef ELEMENT_T
 #define ELEMENT_T 1
-/**
- *	Tipo básico de dado a ser armazenado na estrutura.
- */
+// Tipo básico de dado a ser armazenado na estrutura
 typedef int Element;
 #endif
 
 
 #ifndef NODE_T
 #define NODE_T 1
-/**
- *	Estrutura básica de um Nó
- */
+// Estrutura básica de um Nó
 typedef struct Node_ {
-	Element		value;		//armazena o valor contido neste nó
-	struct Node_*	next;		//apontador para o proximo nó; possivelmente null
+	Element value; //armazena o valor contido neste nó
+
+	//apontador para o proximo nó; possivelmente null
+	struct Node_*	next;
 } Node;
 #endif
 
-/**
- *	Cria um nó com valor v e next como próximo nó
- */
-Node* makeNode(Element v, Node *next);  	
+// Cria um nó com valor v e next como próximo nó
+Node* makeNode(Element v, Node *next);
 
-/**
- *	Retorna o valor contido no nó n
- */
-Element getValue(Node *n);	
+// Retorna o valor contido no nó n
+Element getValue(Node *n);
 
-/**
- *	Retorna o apontador para o próximo nó
- */
-Node* getNext(Node *n);		
+// Retorna o apontador para o próximo nó
+Node* getNext(Node *n);
 
-/**
- *	Altera o valor contido no nó n
- */
-Node* setValue(Node *n, Element v);	
+// Altera o valor contido no nó n
+Node* setValue(Node *n, Element v);
 
-/**
- *	Faz o apontador de próximo de nó n apontar para newNext
- */
-Node* setNext(Node *n, Node *newNext);	
+// Faz o apontador de próximo de nó n apontar para newNext
+Node* setNext(Node *n, Node *newNext);
