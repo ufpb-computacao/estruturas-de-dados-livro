198ccd56eb6cb2e03a0bfd7805345baf229901c1
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2014-05-13 09:58:03 -0300

    Possibilitando composição do livro #10
    Remoção dos tabs no arquivo array.h

--- livro/capitulos/code/capitulo-03/array.h
+++ livro/capitulos/code/capitulo-03/array.h
@@ -6,19 +6,19 @@ typedef int Element;
 #endif
 
 /**
- *	Cria um arranjo de n elementos indexados de 0 a n - 1, inclusive. 
+ * Cria um arranjo de n elementos indexados de 0 a n - 1, inclusive. 
  */
 Element * makeArray(int n);
 
-
 /**
- *	Retorna o valor do elemento de índice index.
- *	O valor de index precisar estar no intervalo 0 <= index <= (n - 1)
+ * Retorna o valor do elemento de índice index.
+ * O valor de index precisar estar no intervalo 0 <= index <= (n - 1)
  */
 Element getValueAt(Element *, int index);
 
-
 /**
- *	Altera o valor do elemento de índice index no arranjo para o valor newValue.
+ * Altera o valor do elemento de índice index no arranjo para o 
+ * valor newValue.
  */
 void setValueAt(Element *, int index, Element newValue);
+
--- livro/capitulos/code/capitulo-04/iterator.h
+++ livro/capitulos/code/capitulo-04/iterator.h
@@ -1,22 +1,22 @@
 /**
- *	Retorna o elemento da lista apontado por este iterador. Tem complexidade O(1).
+ * Retorna o elemento da lista apontado por este iterador. Tem complexidade O(1).
  */
 Element getElement(Iterator it);
 
 /**
- *	Altera o valor do elemento apontado por este iterador. 
- *	Tem complexidade O(1).
+ * Altera o valor do elemento apontado por este iterador. 
+ * Tem complexidade O(1).
  */
 void setElement(Iterator it, Element newElement);
 
 /**
- *	Faz o iterador apontar para o próximo elemento da lista. 
- *	Tem complexidade O(1).
+ * Faz o iterador apontar para o próximo elemento da lista. 
+ * Tem complexidade O(1).
  */
 Iterator moveNext(Iterator it);
 
 /**
- *	Retorna 1 se os dois iteradores apontam para o mesmo elemento da lista.
- *	Tem complexidade O(1).
+ * Retorna 1 se os dois iteradores apontam para o mesmo elemento da lista.
+ * Tem complexidade O(1).
  */
 int equal(Iterator it0, Iterator it1);
