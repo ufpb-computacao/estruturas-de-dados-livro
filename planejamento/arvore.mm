dea25a1a31e7096a510c3078bd8ae2e0c37f866d
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2015-01-13 17:59:15 -0300

    arvore-obj1-finalizado

---
+++ planejamento/arvore.mm
@@ -0,0 +1,291 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#xc1;rvore" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1416332796867"><hook NAME="MapStyle" zoom="1.331">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<font SIZE="14" BOLD="false"/>
+<node TEXT="Conhecimento&#xa;Efetivo/factual" POSITION="right" ID="ID_1648591709" CREATED="1416262186614" MODIFIED="1416332591339" MOVED="1416262194013">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Conhecimento da Terminologia" ID="ID_1494508885" CREATED="1416262186614" MODIFIED="1416332563183">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="&#xc1;rvore" ID="ID_1848262471" CREATED="1416262548228" MODIFIED="1416332563185">
+<font BOLD="false"/>
+</node>
+<node TEXT="N&#xf3;" ID="ID_1136653049" CREATED="1416262554930" MODIFIED="1416332563189">
+<font BOLD="false"/>
+</node>
+<node TEXT="Aresta" ID="ID_538347424" CREATED="1416272016288" MODIFIED="1416332563192">
+<font BOLD="false"/>
+</node>
+<node TEXT="Pai/Filho/Irm&#xe3;o" ID="ID_731937110" CREATED="1416262869980" MODIFIED="1416332563195">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Conhecimento de detalhes&#xa;e elementos espec&#xed;ficos" ID="ID_169738623" CREATED="1416262186614" MODIFIED="1416332616821">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Raiz" ID="ID_367339785" CREATED="1416262597141" MODIFIED="1416332563198">
+<font BOLD="false"/>
+</node>
+<node TEXT="Folha/Terminais" ID="ID_1916098771" CREATED="1416262613758" MODIFIED="1416332563200">
+<font BOLD="false"/>
+</node>
+<node TEXT="Grau de um n&#xf3;" ID="ID_390361849" CREATED="1416264577255" MODIFIED="1416332563204">
+<font BOLD="false"/>
+</node>
+<node TEXT="N&#xed;vel" ID="ID_1983050523" CREATED="1416263162567" MODIFIED="1416332563207" MOVED="1416264683337">
+<font BOLD="false"/>
+</node>
+<node TEXT="Tamanho da &#xe1;rvore" ID="ID_520615793" CREATED="1416262660125" MODIFIED="1416332563210">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento&#xa;Conceitual" POSITION="right" ID="ID_339503537" CREATED="1416262186614" MODIFIED="1416332596837" MOVED="1416262194030">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Conhecimento de&#xa;classifica&#xe7;&#xe3;o e categoriza&#xe7;&#xe3;o" ID="ID_987255543" CREATED="1416262186623" MODIFIED="1416332719208">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Quantidade de filhos" ID="ID_1789658763" CREATED="1416263806915" MODIFIED="1416332563218">
+<font BOLD="false"/>
+<node TEXT="&#xc1;rvore Bin&#xe1;ria" ID="ID_312650101" CREATED="1416262985280" MODIFIED="1416332563220" MOVED="1416263816187">
+<font BOLD="false"/>
+</node>
+<node TEXT="&#xc1;rvore Bin&#xe1;ria de busca" ID="ID_269456537" CREATED="1416332679094" MODIFIED="1416332687326"/>
+<node TEXT="&#xc1;rvore gen&#xe9;rica" ID="ID_171203401" CREATED="1416263779072" MODIFIED="1416332563224" MOVED="1416263816169">
+<font BOLD="false"/>
+</node>
+<node TEXT="&#xc1;rvore B" ID="ID_1513384323" CREATED="1416269620684" MODIFIED="1416332563226">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Distribui&#xe7;&#xe3;o dos n&#xf3;s" ID="ID_1014014916" CREATED="1416271738016" MODIFIED="1416332563227">
+<font BOLD="false"/>
+<node TEXT="&#xc1;rvore cheia" ID="ID_96292681" CREATED="1416271755480" MODIFIED="1416332563229">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento de&#xa;princ&#xed;pios e generaliza&#xe7;&#xf5;es" ID="ID_1934703831" CREATED="1416262186623" MODIFIED="1416332725880">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Recursividade" ID="ID_1311808830" CREATED="1416264800009" MODIFIED="1416332563232">
+<font BOLD="false"/>
+<node TEXT="Sub-&#xe1;rvore" ID="ID_423205897" CREATED="1416263404822" MODIFIED="1416332563234" MOVED="1416264418424">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento de teorias,&#xa;modelos e estruturas" ID="ID_28948528" CREATED="1416262186623" MODIFIED="1416332734322">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Quantidade de elementos de um n&#xed;vel" ID="ID_1697228486" CREATED="1416262943750" MODIFIED="1416332563237">
+<font BOLD="false"/>
+</node>
+<node TEXT="&#xc1;rvore balanceada" ID="ID_1374086938" CREATED="1416263228244" MODIFIED="1416332563238" MOVED="1416263259057">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento&#xa;Procedural" POSITION="right" ID="ID_1668209632" CREATED="1416262186623" MODIFIED="1416332601538" MOVED="1416262194044">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Conhecimento de conte&#xfa;dos espec&#xed;ficos,&#xa;habilidades e algoritmos" ID="ID_396245448" CREATED="1416262186623" MODIFIED="1416333136562">
+<font BOLD="false"/>
+<node TEXT="Opera&#xe7;&#xf5;es b&#xe1;sicas" ID="ID_999580871" CREATED="1416267736549" MODIFIED="1416332563249" MOVED="1416267881908">
+<font BOLD="false"/>
+<node TEXT="Criar" ID="ID_1097751569" CREATED="1416265763917" MODIFIED="1416332563250" MOVED="1416267756888">
+<font BOLD="false"/>
+</node>
+<node TEXT="Esvaziar" ID="ID_1307412812" CREATED="1416265797937" MODIFIED="1416332563252" MOVED="1416267756897">
+<font BOLD="false"/>
+</node>
+<node TEXT="Inserir" ID="ID_246400449" CREATED="1416265802162" MODIFIED="1416332563254" MOVED="1416267756907">
+<font BOLD="false"/>
+</node>
+<node TEXT="Remover n&#xf3;" ID="ID_822029136" CREATED="1416265805851" MODIFIED="1416332563256" MOVED="1416267756918">
+<font BOLD="false"/>
+<node TEXT="Zero filhos" ID="ID_708258094" CREATED="1416266018389" MODIFIED="1416332563257">
+<font BOLD="false"/>
+</node>
+<node TEXT="1 filho" ID="ID_1849297344" CREATED="1416266021958" MODIFIED="1416332563259">
+<font BOLD="false"/>
+</node>
+<node TEXT="2 filhos" ID="ID_1689109776" CREATED="1416266025786" MODIFIED="1416332563260">
+<font BOLD="false"/>
+<node TEXT="fus&#xe3;o" ID="ID_1436400440" CREATED="1416266043954" MODIFIED="1416332563262">
+<font BOLD="false"/>
+</node>
+<node TEXT="c&#xf3;pia" ID="ID_883042939" CREATED="1416266049618" MODIFIED="1416332563264">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Caminho/Percurso" ID="ID_894432602" CREATED="1416265359885" MODIFIED="1416332563265">
+<font BOLD="false"/>
+<node TEXT="Ordem da raiz" ID="ID_125381234" CREATED="1416265512625" MODIFIED="1416332563266">
+<font BOLD="false"/>
+<node TEXT="pr&#xe9;-ordem" ID="ID_1406301935" CREATED="1416265409971" MODIFIED="1416332563268" MOVED="1416265530966">
+<font BOLD="false"/>
+</node>
+<node TEXT="ordem" ID="ID_797128050" CREATED="1416265415803" MODIFIED="1416332563270" MOVED="1416265530984">
+<font BOLD="false"/>
+</node>
+<node TEXT="p&#xf3;s-ordem" ID="ID_230500456" CREATED="1416265449917" MODIFIED="1416332563271" MOVED="1416265530994">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Largura ou n&#xed;vel" ID="ID_938752632" CREATED="1416265502902" MODIFIED="1416332563273" MOVED="1416265533632">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Buscar" ID="ID_1441120488" CREATED="1416265808789" MODIFIED="1416332563274" MOVED="1416267890690">
+<font BOLD="false"/>
+<node TEXT="Maior elemento" ID="ID_62241728" CREATED="1416266295366" MODIFIED="1416332563275">
+<font BOLD="false"/>
+</node>
+<node TEXT="Menor elemento" ID="ID_1575647095" CREATED="1416266300641" MODIFIED="1416332563277">
+<font BOLD="false"/>
+</node>
+<node TEXT="Contar o n&#xfa;mero de elementos" ID="ID_1282485908" CREATED="1416266306864" MODIFIED="1416332563279">
+<font BOLD="false"/>
+</node>
+<node TEXT="Imprimir elementos em ordem crescente/decrescente" ID="ID_1531519854" CREATED="1416266368496" MODIFIED="1416332563281">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Conhecimento de teorias,&#xa;modelos e estruturas" ID="ID_20230244" CREATED="1416262186623" MODIFIED="1416340325536">
+<font BOLD="false"/>
+<node TEXT="Quantidade de elementos de um n&#xed;vel" ID="ID_1392794318" CREATED="1416262943750" MODIFIED="1416332563237">
+<font BOLD="false"/>
+</node>
+<node TEXT="&#xc1;rvore balanceada" ID="ID_1519274715" CREATED="1416263228244" MODIFIED="1416332563238" MOVED="1416263259057">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento de t&#xe9;cnicas espec&#xed;ficas&#xa;e m&#xe9;todos" ID="ID_292325309" CREATED="1416262186623" MODIFIED="1416332748379">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Rota&#xe7;&#xf5;es" ID="ID_1634668510" CREATED="1416267150982" MODIFIED="1416332563286">
+<font BOLD="false"/>
+</node>
+<node TEXT="Balanceamento" ID="ID_1148170582" CREATED="1416265753791" MODIFIED="1416332563288">
+<font BOLD="false"/>
+<node TEXT="Est&#xe1;tico" ID="ID_1621753323" CREATED="1416266805622" MODIFIED="1416332563289">
+<font BOLD="false"/>
+</node>
+<node TEXT="Din&#xe2;mico" ID="ID_1457321462" CREATED="1416266814386" MODIFIED="1416332563291">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento de crit&#xe9;rios e percep&#xe7;&#xe3;o de como&#xa;e quando usar um procedimento espec&#xed;fico." ID="ID_224718124" CREATED="1416262186623" MODIFIED="1416332563292">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Efici&#xea;ncia de busca" ID="ID_1572389555" CREATED="1416266640166" MODIFIED="1416332563297">
+<font BOLD="false"/>
+</node>
+<node TEXT="Necessidade de balancear arvore binaria" ID="ID_1877931757" CREATED="1416266724484" MODIFIED="1416332563298">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento&#xa;Metacognitivo" POSITION="right" ID="ID_959800066" CREATED="1416262186623" MODIFIED="1416332605865" MOVED="1416262194056">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Conhecimento estrat&#xe9;gico" ID="ID_406202476" CREATED="1416262186629" MODIFIED="1416332563301">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Armazenamento" ID="ID_608517208" CREATED="1416266910059" MODIFIED="1416332563399">
+<font BOLD="false"/>
+<node TEXT="Lista" ID="ID_1541706742" CREATED="1416266970677" MODIFIED="1416332563401">
+<font BOLD="false"/>
+</node>
+<node TEXT="Vetor" ID="ID_87315518" CREATED="1416266974646" MODIFIED="1416332563403">
+<font BOLD="false"/>
+</node>
+<node TEXT="Arquivo" ID="ID_1949362173" CREATED="1416266991795" MODIFIED="1416332563405">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Aplica&#xe7;&#xf5;es" ID="ID_593903819" CREATED="1416267371595" MODIFIED="1416332563406">
+<font BOLD="false"/>
+<node TEXT="Sistema de Arquivos" ID="ID_900374159" CREATED="1416269467371" MODIFIED="1417634138504">
+<font BOLD="false"/>
+</node>
+<node TEXT="Arquivos de Banco de Dados" ID="ID_394827054" CREATED="1417634140318" MODIFIED="1417634167427"/>
+<node TEXT="XML, JSON" ID="ID_1570707816" CREATED="1417634206277" MODIFIED="1417634232959"/>
+<node TEXT="Compiladores" ID="ID_1492728677" CREATED="1416267411998" MODIFIED="1416332563409">
+<font BOLD="false"/>
+</node>
+<node TEXT="Game trees: possibilidade de jogadas" ID="ID_482001878" CREATED="1416267386532" MODIFIED="1416332563407">
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+<node TEXT="Conhecimento sobre atividades cognitivas incluindo&#xa;contextos preferenciais e situa&#xe7;&#xf5;es de aprendizagem (estilos)" ID="ID_526795516" CREATED="1416262186629" MODIFIED="1416332563412">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+<node TEXT="Como ensinar?" ID="ID_918000871" CREATED="1416272577961" MODIFIED="1416332563416">
+<font BOLD="false"/>
+</node>
+</node>
+<node TEXT="Autoconhecimento" ID="ID_123912535" CREATED="1416262186629" MODIFIED="1416332563418">
+<edge COLOR="#000000"/>
+<font BOLD="false"/>
+</node>
+</node>
+</node>
+</map>
